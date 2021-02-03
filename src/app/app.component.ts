54e6057e29e275f3367edcd150f6536756201c99
Lee Norris <lee.norris@arcadia.io>
2021-02-03 06:29:28 -0500

    fix: app module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FirestoreService } from './firestore.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'onivoro';
+  title = 'things';
+  constructor(private readonly firestoreService: FirestoreService) {}
+  things$ = this.firestoreService.db.collection(this.title).valueChanges();
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { firebaseConfig} from './firebase-config';
+import { FirestoreService } from './firestore.service';
+import { AppComponent } from './app.component';
 const imports = [
   AngularFireModule.initializeApp(firebaseConfig),
   AngularFirestoreModule,
+  BrowserModule
 ];
 @NgModule({
   imports,
-  exports: [
-    AngularFireModule,
-    AngularFirestoreModule
-  ]
+  providers: [FirestoreService],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
