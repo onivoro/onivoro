8bebee9cb24560a52028d194911000e46093f611
Lee Norris <lee.norris@arcadia.io>
2021-02-12 21:14:33 -0500

    chore: add styles

--- src/app/module/app-routing.module.ts
+++ src/app/module/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router'
-import { AppComponent } from '../app/app.component';
 import { home } from '../constant/links';
+import { HomePageComponent } from '../page/home-page/home-page.component';
 import { MaterialModule } from './material.module';
 
 const pathMatch = 'full';
 
 const routes: Routes = [
-  { path: home.slug, component: AppComponent, pathMatch },
+  { path: home.slug, component: HomePageComponent, pathMatch },
   { path: '', redirectTo: home.slug, pathMatch },
 ];
 
--- src/app/module/app.module.ts
+++ src/app/module/app.module.ts
@@ -4,22 +4,29 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from '../app/app.component';
 import { LogoComponent } from '../component/logo/logo.component';
 import { NavigationComponent } from '../component/navigation/navigation.component';
+import { HomePageComponent } from '../page/home-page/home-page.component';
 import { ViewportService } from '../service/viewport.service';
 import { AppRoutingModule } from './app-routing.module';
 import { MaterialModule } from './material.module';
-const imports = [
-  BrowserModule,
-  MaterialModule,
-  AppRoutingModule,
-  BrowserAnimationsModule
+const components = [AppComponent,
+  LogoComponent,
+  NavigationComponent
+];
+
+const pages = [
+  HomePageComponent
 ];
 @NgModule({
   declarations: [
-    AppComponent,
-    LogoComponent,
-    NavigationComponent
+    ...components,
+    ...pages
   ],
-  imports,
+  imports: [
+    BrowserModule,
+    MaterialModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
   providers: [ViewportService],
   bootstrap: [AppComponent]
 })
