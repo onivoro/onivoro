b377b7fc6df443a24332e3341b4108deee921bcf
icedlee337 <lee.e.norris@gmail.com>
2022-08-25 18:41:45 -0500

    chore: basic app worx

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/onivoro.com'),
+      dir: require('path').join(__dirname, './coverage/onivoro'),
       subdir: '.',
       reporters: [
         { type: 'html' },
