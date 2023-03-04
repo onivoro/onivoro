29dc7853c9e78be39980155f366ed9fc403c0cf9
icedlee337 <lee.e.norris@gmail.com>
2023-03-04 16:42:15 -0600

    chore: add isomorphic-common module

---
+++ libs/isomorphic-common/src/index.ts
@@ -0,0 +1,4 @@
+export * from './lib/strings/snake-case.function';
+export * from './lib/strings/unicode-words.const';
+export * from './lib/strings/words.function';
+
---
+++ libs/isomorphic-common/src/lib/strings/snake-case.function.ts
@@ -0,0 +1,34 @@
+// source https://github.com/lodash/lodash/blob/master/snakeCase.js
+
+import { words } from "./words.function";
+
+/**
+ * Converts `string` to
+ * [snake case](https://en.wikipedia.org/wiki/Snake_case).
+ *
+ * @since 3.0.0
+ * @category String
+ * @param {string} [string=''] The string to convert.
+ * @returns {string} Returns the snake cased string.
+ * @see camelCase, lowerCase, kebabCase, startCase, upperCase, upperFirst
+ * @example
+ *
+ * snakeCase('Foo Bar')
+ * // => 'foo_bar'
+ *
+ * snakeCase('fooBar')
+ * // => 'foo_bar'
+ *
+ * snakeCase('--FOO-BAR--')
+ * // => 'foo_bar'
+ *
+ * snakeCase('foo2bar')
+ * // => 'foo_2_bar'
+ */
+export function snakeCase(string) {
+  return (
+    words(string.replace(/['\u2019]/g, '')).reduce((result, word, index) => (
+      result + (index ? '_' : '') + word.toLowerCase()
+    ), '')
+  );
+}
