1ad85e2dfbd989ee57bdf4bcd19846defc66674c
icedlee337 <lee.e.norris@gmail.com>
2023-05-21 00:34:43 -0400

    feat: server-twilio

---
+++ libs/server-twilio/src/lib/api-twilio.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { moduleFactory } from '@onivoro/server-common';
+import { TwilioService } from './services/twilio.service';
+import { ServerTwilioConfig } from './classes/server-twilio-config.class';
+import { Twilio } from 'twilio';
+
+@Module(
+  moduleFactory({
+    providers: [TwilioService],
+  })
+)
+export class ServerTwilioModule {
+  static configure(config: ServerTwilioConfig) {
+    return moduleFactory({
+      module: ServerTwilioModule,
+      providers: [
+        {
+          provide: TwilioService,
+          useFactory: () => new TwilioService(
+            config,
+            new Twilio(
+              config.TWILIO_ACCOUNT_SID,
+              config.TWILIO_AUTH_TOKEN,
+            )
+          )
+        }
+      ]
+    });
+  }
+}
