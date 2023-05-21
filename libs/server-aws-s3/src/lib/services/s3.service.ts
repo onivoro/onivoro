ef3fbe9a2e4049e14ffc6dd069d3715bb9ed9c65
icedlee337 <lee.e.norris@gmail.com>
2023-05-21 04:04:07 -0400

    chore: more robust s3 client

--- libs/server-aws-s3/src/lib/services/s3.service.ts
+++ libs/server-aws-s3/src/lib/services/s3.service.ts
@@ -1,34 +1,62 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { S3 } from 'aws-sdk';
 import { PassThrough } from 'stream';
 import { ServerAwsS3Config } from '../classes/server-aws-s3-config.class';
 import { IS3UploadResponse } from '../interfaces/s3-upload-response.interface';
 
+export type TS3Params = {
+  Key: string,
+  Bucket?: string | null | undefined
+};
+
 @Injectable()
 export class S3Service {
   constructor(private config: ServerAwsS3Config, private s3: S3) { }
 
-  async upload(params: { Key: string; Bucket?: string; Body: PassThrough; }): Promise<IS3UploadResponse> {
+  async upload(params: TS3Params & { Body: PassThrough }): Promise<IS3UploadResponse> {
     // todo: sanitize filename here before uploading
     return await this.s3.upload(this.addDefaultBucket(params)).promise();
   }
 
-  async getPresignedUrl(Key: string, fileName?: string, inline = false, Bucket?: string) {
-    if (!fileName) {
-      fileName = Key.split('/').pop();
-    }
-
-    const url = await this.s3.getSignedUrlPromise('getObject', {
-      Bucket: this.getBucket(Bucket),
-      Expires: 10,
-      Key,
-      ResponseContentDisposition: inline ? 'inline' : `attachment; filename="${fileName}"`,
-    });
-
-    return url;
+  async getPresignedUrl(params: TS3Params & { Expires: number, ResponseContentDisposition: string }) {
+    return await this.s3.getSignedUrlPromise('getObject', this.addDefaultBucket(params));
   }
 
-  private addDefaultBucket(params: { Key: string; Bucket?: string; Body: PassThrough; }): S3.PutObjectRequest {
+  async getFile(params: TS3Params) {
+    if (!params?.Key) {
+      throw new BadRequestException(`${S3Service.name}.${S3Service.prototype.getFile.name} requires a valid S3 key`)
+    }
+
+    const response = await this.s3.getObject(this.addDefaultBucket(params)).promise();
+
+    return response.Body;
+  }
+
+  async getDownloadUrl(params: TS3Params & {fileName?: string | null | undefined}) {
+    if(!params || !params?.Key) {
+      throw new BadRequestException(`${S3Service.name}.${S3Service.prototype.getDownloadUrl.name} requires a valid S3 key`)
+    }
+
+    return await this.getPresignedUrl({
+      ...this.addDefaultBucket(params),
+      Expires: 100,
+      ResponseContentDisposition: `attachment; filename="${params.fileName || params.Key.split('/').pop()}"`
+    });
+  }
+
+  async getAssetUrl(params: TS3Params & { Expires?: number | null | undefined }) {
+    if(!params || !params?.Key) {
+      throw new BadRequestException(`${S3Service.name}.${S3Service.prototype.getAssetUrl.name} requires a valid S3 key`)
+    }
+
+    return await this.getPresignedUrl({
+      ...this.addDefaultBucket(params),
+      Expires: params.Expires || 10_000,
+      ResponseContentDisposition: 'inline'
+    })
+  }
+
+  private addDefaultBucket(params: TS3Params): S3.PutObjectRequest {
     return { ...params, Bucket: this.getBucket(params?.Bucket) };
   }
 
