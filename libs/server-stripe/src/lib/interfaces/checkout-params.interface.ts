4156c0f9e8fb63ad97f76633d07e1e07860c7796
icedlee337 <lee.e.norris@gmail.com>
2023-05-21 02:26:47 -0400

    chore: create type for stripe checkout params

---
+++ libs/server-stripe/src/lib/interfaces/checkout-params.interface.ts
@@ -0,0 +1,9 @@
+import Stripe from "stripe";
+
+export type ICheckoutParams = Pick<Stripe.Checkout.SessionCreateParams,
+    'success_url' |
+    'cancel_url' |
+    'customer_email' |
+    'client_reference_id' |
+    'line_items'
+>;
--- libs/server-stripe/src/lib/services/stripe.service.ts
+++ libs/server-stripe/src/lib/services/stripe.service.ts
@@ -1,25 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import Stripe from 'stripe';
+import { ICheckoutParams } from '../interfaces/checkout-params.interface';
 
 @Injectable()
 export class StripeService {
 
-  async createCheckoutSession(
-    success_url: string,
-    cancel_url: string,
-    customer_email: string,
-    client_reference_id: string,
-    line_items: Stripe.Checkout.SessionCreateParams['line_items'],
-    ) {
-    const params: Stripe.Checkout.SessionCreateParams = {
-      payment_method_types: ['card'],
-      mode: 'payment',
-      success_url,
-      cancel_url,
-      line_items,
-      customer_email,
-      client_reference_id,
-    };
+  async createCheckoutSession(params: ICheckoutParams) {
 
     return await this.stripe.checkout.sessions.create(params);
   }
