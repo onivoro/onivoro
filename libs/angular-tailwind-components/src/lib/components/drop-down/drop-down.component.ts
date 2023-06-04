f707789fe8c0ca2bfe9ea2b2821b2e15f843aabe
icedlee337 <lee.e.norris@gmail.com>
2023-06-03 23:45:31 -0400

    chore: add ability to position drop-down precisely

--- libs/angular-tailwind-components/src/lib/components/drop-down/drop-down.component.ts
+++ libs/angular-tailwind-components/src/lib/components/drop-down/drop-down.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, ViewEncapsulation } from "@angular/core";
 
 @Component({
     selector: 'onivoro-drop-down',
     templateUrl: './drop-down.component.html',
     styleUrls: ['./drop-down.component.scss'],
+    encapsulation: ViewEncapsulation.None
 })
 export class DropDownComponent {
     open = false;
