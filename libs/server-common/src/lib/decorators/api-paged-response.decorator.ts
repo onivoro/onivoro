e6011f7cabfe948b9d5a6ab4ee858b4d2f724ca9
icedlee337 <lee.e.norris@gmail.com>
2023-07-08 19:15:16 -0400

    chore:(server-common): use apiResponse instead of apiOkResponse

--- libs/server-common/src/lib/decorators/api-paged-response.decorator.ts
+++ libs/server-common/src/lib/decorators/api-paged-response.decorator.ts
@@ -1,11 +1,11 @@
 import { Type, applyDecorators } from "@nestjs/common";
-import { ApiExtraModels, ApiOkResponse, getSchemaPath } from "@nestjs/swagger";
+import { ApiExtraModels, ApiResponse, getSchemaPath } from "@nestjs/swagger";
 import { PagedResponseDto } from "../dtos/paged-response.dto";
 
 export const ApiPagedResponse = <TEntity extends Type<unknown>>(entityDto: TEntity) =>
     applyDecorators(
         ApiExtraModels(PagedResponseDto, entityDto),
-        ApiOkResponse({
+        ApiResponse({
             schema: {
                 allOf: [
                     { $ref: getSchemaPath(PagedResponseDto) },
