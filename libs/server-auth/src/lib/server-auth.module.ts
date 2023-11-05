4bbf4c308d03cb2a9d2bdac4a6f7408a4e27326a
icedlee337 <lee.e.norris@gmail.com>
2023-11-04 21:54:06 -0500

    chore(server-auth): add tokenBuilder to module.configure

--- libs/server-auth/src/lib/server-auth.module.ts
+++ libs/server-auth/src/lib/server-auth.module.ts
@@ -6,10 +6,11 @@ import { LoginController } from './controllers/login.controller';
 import { LoginService } from './services/login.service';
 import { TokenValidationService } from './services/token-validation.service';
 import { ServerAuthConfig } from './classes/server-auth-config.class';
+import { TokenBuilder } from './classes/token-builder.class';
 
 @Module({})
 export class ServerAuthModule {
-  static configure(config: ServerAuthConfig) {
+  static configure<TAccessToken>(config: ServerAuthConfig, tokenBuilder: TokenBuilder<TAccessToken>) {
     return moduleFactory({
       module: ServerAuthModule,
       imports: [
@@ -21,6 +22,10 @@ export class ServerAuthModule {
         {
           provide: ServerAuthConfig,
           useValue: config
+        },
+        {
+          provide: TokenBuilder,
+          useValue: tokenBuilder
         }
       ],
       controllers: [
