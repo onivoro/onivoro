f5ca40377dfe2c2b881ce725cffe48d51a28c8e8
icedlee337 <lee.e.norris@gmail.com>
2023-11-05 12:37:30 -0600

    chore(server-auth): remove api-credentials.interface

--- libs/server-auth/src/lib/api-credentials.interface.ts
+++
@@ -1,4 +0,0 @@
-export interface IApiCredentials {
-    apiId: string;
-    apiKey: string;
-}
--- libs/server-auth/src/lib/classes/token-builder.class.ts
+++ libs/server-auth/src/lib/classes/token-builder.class.ts
@@ -1,13 +1,13 @@
 import { Injectable } from "@nestjs/common";
-import { IApiCredentials } from "../api-credentials.interface";
 import { LoginWithEmailAndPasswordDto } from "../dtos/login-with-email-and-password.dto";
+import { LoginWithApiCredentialsDto } from "../dtos/login-with-api-credentials.dto";
 
 @Injectable()
 export class TokenBuilder<TAccessToken> {
     async byEmailAndPassword(creds: LoginWithEmailAndPasswordDto): Promise<TAccessToken> {
         return {} as TAccessToken;
     }
-    async byApiCredentials(creds: IApiCredentials): Promise<TAccessToken> {
+    async byApiCredentials(creds: LoginWithApiCredentialsDto): Promise<TAccessToken> {
         return {} as TAccessToken;
     }
 }
