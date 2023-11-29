128691a12fe5a89a79a5cab1a26bcf854cbb1a46
icedlee337 <lee.e.norris@gmail.com>
2023-11-29 16:45:17 -0600

    feat(server-build): add command to empty/delete bucket(s)

--- libs/server-build/src/lib/server-build.module.ts
+++ libs/server-build/src/lib/server-build.module.ts
@@ -6,11 +6,13 @@ import { DeleteLambda } from './commands/delete-lambda.command';
 import { OpenapiGen } from './commands/openapi-gen.command';
 import { OpenapiGenV2 } from './commands/openapi-gen-v2.command';
 import { KillTasks } from './commands/kill-tasks.command';
+import { DeleteBuckets } from './commands/delete-buckets.command';
 
 export const providers = [
   DeployLambda,
   RedeployLambda,
   DeployImage,
+  DeleteBuckets,
   DeleteLambda,
   OpenapiGen,
   OpenapiGenV2,
