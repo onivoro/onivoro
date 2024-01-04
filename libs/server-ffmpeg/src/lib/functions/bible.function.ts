f4375cd042de39997ffbfbe226209b9dbaa41052
icedlee337 <lee.e.norris@gmail.com>
2024-01-04 12:49:49 -0600

    fix(server-ffmpeg): temp file name uses underscores

--- libs/server-ffmpeg/src/lib/functions/bible.function.ts
+++ libs/server-ffmpeg/src/lib/functions/bible.function.ts
@@ -2,11 +2,11 @@ import { concatFiles } from "./concat-files.function";
 import { overlay } from "./overlay.function";
 
 const mp3 = '.mp3';
-const musicSuffix = 'music-output';
+const musicSuffix = 'music_output';
 
 export function bible(musicFiles: string[], bibleTrack: string, loopCount: number, volumeRatio: number, output: string) {
-    const musicOutput: string = `${Date.now()}-${musicSuffix}${mp3}`;
-    const musicOutputLooped: string = musicOutput.replace(mp3, `-looped${mp3}`);
+    const musicOutput: string = `${Date.now()}_${musicSuffix}${mp3}`;
+    const musicOutputLooped: string = musicOutput.replace(mp3, `_looped${mp3}`);
     concatFiles(musicFiles, musicOutput);
     concatFiles((new Array().fill(loopCount)).map(() => musicOutput), musicOutputLooped);
     overlay(bibleTrack, musicOutputLooped, volumeRatio, output);
