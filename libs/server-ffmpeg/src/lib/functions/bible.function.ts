3fabbb0eaa877026aa1e29d2627baabaf7923c0e
icedlee337 <lee.e.norris@gmail.com>
2024-01-04 12:45:22 -0600

    fix(server-ffmpeg): temp file name

--- libs/server-ffmpeg/src/lib/functions/bible.function.ts
+++ libs/server-ffmpeg/src/lib/functions/bible.function.ts
@@ -5,7 +5,7 @@ const mp3 = '.mp3';
 const musicSuffix = 'music-output';
 
 export function bible(musicFiles: string[], bibleTrack: string, loopCount: number, volumeRatio: number, output: string) {
-    const musicOutput: string = `${new Date().toISOString()}-${musicSuffix}${mp3}`;
+    const musicOutput: string = `${Date.now()}-${musicSuffix}${mp3}`;
     const musicOutputLooped: string = musicOutput.replace(mp3, `-looped${mp3}`);
     concatFiles(musicFiles, musicOutput);
     concatFiles((new Array().fill(loopCount)).map(() => musicOutput), musicOutputLooped);
